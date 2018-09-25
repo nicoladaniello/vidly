f4e66efe202a0f82b19a359b170255492ab6941b
Nicola D'Aniello <nicola.danie@gmail.com>
2018-09-25 16:28:34 +0100

    Raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,6 @@ class Counter extends Component {
     value: this.props.value
   };
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
@@ -19,6 +14,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,16 +5,25 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 4 },
-      { id: 1, value: 0 },
-      { id: 1, value: 0 },
-      { id: 1, value: 0 }
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = () => {
+    console.log("Delete called.");
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+          />
         ))}
       </div>
     );
