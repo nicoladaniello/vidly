39eaf3c4b02aeae696406c68e41a2390cebe3ede
Nicola D'Aniello <nicola.danie@gmail.com>
2018-09-25 17:34:38 +0100

    lifting the state up

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
-import Counters from "./components/counters";
 
 library.add(faStroopwafel);
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
