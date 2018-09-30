731841313ab19e21d3e369decabc7d663b97b68c
Nicola D'Aniello <nicola.danie@gmail.com>
2018-09-30 12:55:40 +0100

    filtering - adding all movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,10 +15,9 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({
-      movies: getMovies(),
-      genres: getGenres()
-    });
+    const genres = [{ name: "All Genres" }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleLike = movie => {
@@ -39,7 +38,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = genre => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   render() {
@@ -54,9 +53,10 @@ class Movies extends Component {
 
     if (count === 0) return <p>No movies to show</p>;
 
-    const filtered = selectedGenre
-      ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-      : allMovies;
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+        : allMovies;
 
     const movies = paginate(filtered, currentPage, pageSize);
 
