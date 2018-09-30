50115a38d9c25155ec099a6e21303cadb6b778be
Nicola D'Aniello <nicola.danie@gmail.com>
2018-09-30 18:00:59 +0100

    sorting - unique keys

--- src/components/common/table-body.jsx
+++ src/components/common/table-body.jsx
@@ -7,15 +7,20 @@ class TableBody extends Component {
     return _.get(item, column.path);
   };
 
+  createKey = (item, column) =>
+    item[this.props.valueProperty] + (column.path || column.key);
+
   render() {
     const { data, columns } = this.props;
 
     return (
       <tbody>
         {data.map(item => (
-          <tr>
+          <tr key={item._id}>
             {columns.map(column => (
-              <td>{this.renderCell(item, column)}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
@@ -23,5 +28,9 @@ class TableBody extends Component {
     );
   }
 }
+
+TableBody.defaultProps = {
+  valueProperty: "_id"
+};
 
 export default TableBody;
--- src/components/movies-table.jsx
+++ src/components/movies-table.jsx
@@ -29,7 +29,7 @@ class MoviesTable extends Component {
   ];
 
   render() {
-    const { movies, sortColumn, onDelete, onLike, onSort } = this.props;
+    const { movies, sortColumn, onSort } = this.props;
 
     return (
       <table className="table">
