20dc76a6090343d7ac5838741906be425117ff1b
Nicola D'Aniello <nicola.danie@gmail.com>
2018-10-01 13:46:23 +0100

    validationg form using joi

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -12,8 +12,12 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -28,21 +32,16 @@ class LoginForm extends Component {
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
 
     const errors = {};
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    error.details.map(
+      errorDetail => (errors[errorDetail.path[0]] = errorDetail.message)
+    );
+    return errors;
   };
 
   validateProperty = ({ name, value }) => {
