1d24cbfd900911be84450bdf274f9e7e34818a63
Nicola D'Aniello <nicola.danie@gmail.com>
2018-10-11 17:44:58 +0100

    add log service to httpService

--- src/App.js
+++ src/App.js
@@ -9,8 +9,13 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/not-found";
 import { ToastContainer } from "react-toastify";
+import { init } from "@sentry/browser";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+
+init({
+  dsn: "https://232e8233afc2478a966f70834da9ebd6@sentry.io/1299229"
+});
 
 class App extends Component {
   render() {
