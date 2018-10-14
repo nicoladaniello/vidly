a622932c3e49c9f976b0f4ac40ddc5f2190d57ea
Nicola D'Aniello <nicola.danie@gmail.com>
2018-10-14 11:03:42 +0100

    get the current user

--- src/App.js
+++ src/App.js
@@ -1,27 +1,97 @@
-import React, { Component } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import NavBar from "./components/navbar";
-import LoginForm from "./components/login-form";
-import RegistrationForm from "./components/registration-form";
-import MovieForm from "./components/movie-form";
-import Movies from "./components/movies";
-import Customers from "./components/customers";
-import Rentals from "./components/rentals";
-import NotFound from "./components/not-found";
-import { ToastContainer } from "react-toastify";
-import { init as initLogService } from "./services/logService";
-
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+
+import jwtDecode from "jwt-decode";
+import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Customers from "./components/customers";
+import LoginForm from "./components/login-form";
+import MovieForm from "./components/movie-form";
+import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import NotFound from "./components/not-found";
+import RegistrationForm from "./components/registration-form";
+import Rentals from "./components/rentals";
+import { init as initLogService } from "./services/logService";
 
 initLogService();
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) { }
+  }
+
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
+        <main role="main" className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/registration" component={RegistrationForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
+    const jwt = localStorage.getItem("token");
+    const user = jwtDecode(jwt);
+    this.setState({ user });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={this.state.user} />
+        <main role="main" className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/registration" component={RegistrationForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
+    const jwt = localStorage.getItem("token");
+    const user = jwtDecode(jwt);
+    this.setState({ user });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={this.state.user} />
         <main role="main" className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
